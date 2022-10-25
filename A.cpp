e575c784547eeae7127a25e1661989cd75aee2f1
Fernando Leonel Moschen <64292259+ferchumoschen@users.noreply.github.com>
2022-10-25 01:12:16 -0300

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,14 @@
+/*MIRA POR EJEMPLO si queres hacer 10 numeros en while
+y en for seria asi*/
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main(){
+    int i=20;
+    while(i >= 1){
+    cout << i << endl;
+    i=i-1;
+    }
+}
+
